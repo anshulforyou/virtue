c1716bcbb003aa2f7315e3b167ed0d4db2ee5ee6
Anshul <anshul19023@iiitd.ac.in>
2021-06-26 01:43:18 +0530

    preview to main animation added

--- static/js/preview.js
+++ static/js/preview.js
@@ -57,3 +57,64 @@ localVideo.onplaying = () => {
     canvas.height = localVideo.style.height;
     canvas.width = localVideo.style.width;
 }
+
+var ml4 = {};
+ml4.opacityIn = [0,1];
+ml4.scaleIn = [0.2, 1];
+ml4.scaleOut = 3;
+ml4.durationIn = 800;
+ml4.durationOut = 600;
+ml4.delay = 500;
+
+var mainScreen = document.getElementById('main-screen');
+ml4Screen = document.getElementById('ml4');
+var joinBtn = document.getElementById('btn-join');
+
+joinBtn.addEventListener('click', ()=>{
+    mainScreen.hidden = true;
+    ml4Screen.hidden = false;
+    anime.timeline({loop: false})
+    .add({
+        targets: '.ml4 .letters-1',
+        opacity: ml4.opacityIn,
+        scale: ml4.scaleIn,
+        duration: ml4.durationIn
+    }).add({
+        targets: '.ml4 .letters-1',
+        opacity: 0,
+        scale: ml4.scaleOut,
+        duration: ml4.durationOut,
+        easing: "easeInExpo",
+        delay: ml4.delay
+    }).add({
+        targets: '.ml4 .letters-2',
+        opacity: ml4.opacityIn,
+        scale: ml4.scaleIn,
+        duration: ml4.durationIn
+    }).add({
+        targets: '.ml4 .letters-2',
+        opacity: 0,
+        scale: ml4.scaleOut,
+        duration: ml4.durationOut,
+        easing: "easeInExpo",
+        delay: ml4.delay
+    }).add({
+        targets: '.ml4 .letters-3',
+        opacity: ml4.opacityIn,
+        scale: ml4.scaleIn,
+        duration: ml4.durationIn
+    }).add({
+        targets: '.ml4 .letters-3',
+        opacity: 0,
+        scale: ml4.scaleOut,
+        duration: ml4.durationOut,
+        easing: "easeInExpo",
+        delay: ml4.delay
+    }).add({
+        targets: '.ml4',
+        opacity: 0,
+        duration: 500,
+        delay: 500
+    });
+    setTimeout(() => {document.getElementById('preview-form').submit()}, 5000)
+})
